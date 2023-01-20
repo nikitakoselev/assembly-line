bd92b5212805acc8d518c11ddb3bd13801e004c4
jkosha <nikita.koselev@gmail.com>
2023-01-20 22:21:06 +0000

    basic tests were fulfilled

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,20 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+const CARS_PER_HOUR_SPEED_1: f64 = 221f64;
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let mut result: f64 = 0f64;
+
+    match speed {
+        1 | 2 | 3 | 4 => result = CARS_PER_HOUR_SPEED_1 * speed as f64,
+        5 | 6 | 7 | 8 => result = CARS_PER_HOUR_SPEED_1 * speed as f64 * 0.9,
+        _ => result = CARS_PER_HOUR_SPEED_1 * speed as f64 * 0.77
+    }
+
+    result
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    ((production_rate_per_hour(speed)) / (60 as f64)) as u32
 }
